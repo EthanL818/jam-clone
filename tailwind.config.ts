de1725d39769cebcb47b43b6b588526c20db5454
Ethan Liu <ethanliu818@gmail.com>
2025-01-23 20:14:53 -0500

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,54 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  prefix: "",
+  theme: {
+    extend: {
+      screens: {
+        'sl-screen': '1280px',
+        'ms-screen': '1065px',
+        'xs-screen': '438px',
+        'xxs-screen': '375px'
+
+      },
+      colors: {
+        'blue-dark': '#254A5B',
+        'blue-light': '#CADFFF',
+        'red-light': '#E60000',
+        'red-title': '#FF0000',
+        'green-start': '#43B02A'
+      },
+      container: {
+        center: true,
+        padding: "2rem",
+        screens: {
+          "2xl": "1400px",
+        },
+      },
+      extend: {
+        keyframes: {
+          "accordion-down": {
+            from: { height: "0" },
+            to: { height: "var(--radix-accordion-content-height)" },
+          },
+          "accordion-up": {
+            from: { height: "var(--radix-accordion-content-height)" },
+            to: { height: "0" },
+          },
+        },
+        animation: {
+          "accordion-down": "accordion-down 0.2s ease-out",
+          "accordion-up": "accordion-up 0.2s ease-out",
+        },
+      },
+    },
+    plugins: [require("tailwindcss-animate")],
+  }
+}
+export default config
